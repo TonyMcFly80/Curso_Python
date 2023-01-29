a9eca14663ad70f8f0c22c407a0b3b9e45b2ff96
TonyMcFly80 <123588199+TonyMcFly80@users.noreply.github.com>
2023-01-29 12:18:53 +0100

    Update 07_Conjuntos_Estructuras_de_Datos.py

--- 07_Conjuntos_Estructuras_de_Datos.py
+++ 07_Conjuntos_Estructuras_de_Datos.py
@@ -20,3 +20,35 @@ for l in f:
 
 print(set1)
 
+# Tarea 02
+# Vamos a pedirle al usuario dos palabras y vamos a calcular la intersección de las letras de cada
+# palabra. Para ello habrá que crear dos conjuntos que contendrán, respectivamente, las letras que
+# forman cada palabra.
+
+palabra1 = str(input('Escribe una palabra: '))
+palabra2 = str(input('Escribe otra palabra: '))
+p1 = palabra1.lower()
+p2 = palabra2.lower()
+
+set1 = set()
+set2 = set()
+
+for l in p1:    # Creamos conjunto de la primera palabra.
+
+    if l.isalnum() is True:
+        set1.add(l)
+
+    else:
+        continue
+
+for l in p2:    # Creamos conjunto de la segunda palabra.
+
+    if l.isalnum() is True:
+        set2.add(l)
+
+    else:
+        continue
+
+set3 = set1.intersection(set2)
+
+print(set3)
