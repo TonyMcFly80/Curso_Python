9f1b567c720e0d0dee6905d17676e4072ed3d916
TonyMcFly80 <123588199+TonyMcFly80@users.noreply.github.com>
2023-01-30 15:14:11 +0100

    Update 08_Tuplas_Estructuras_de_Datos.py

--- 08_Tuplas_Estructuras_de_Datos.py
+++ 08_Tuplas_Estructuras_de_Datos.py
@@ -28,3 +28,43 @@ for n in range(1, number + 1):
         tuplas.append(t)
 
 print(tuplas)
+
+# Tarea 02
+# Vamos a pedirle al usuario números enteros del 1 al 10 hasta que introduzca el 0. Para cada uno
+# de esos números, vamos a crear una tupla donde la primera entrada sea el número entero y, la
+# segunda, la palabra 'suspenso', 'aprobado', 'notable' o 'excelente' según el intervalo al que
+# pertenezca el número entero. Vamos a mostrar la tupla recién creada al usuario.
+
+# Las diferentes categorías son:
+
+# suspenso [1,5]
+# aprobado [5,7]
+# notable [7,9]
+# excelente [9,10]
+
+while True:
+    numero = int(input('Escribe un número del 1 al 10 (0 para finalizar el programa): '))
+
+    if numero in range(1, 5):
+        t = (numero, 'Suspenso')
+        print(t)
+
+    elif numero in range(5, 7):
+        t = (numero, 'Aprobado')
+        print(t)
+
+    elif numero in range(7, 9):
+        t = (numero, 'Notable')
+        print(t)
+
+    elif numero in range(9, 11):
+        t = (numero, 'Excelente')
+        print(t)
+
+    elif numero == 0:
+        print('FIN del programa')
+        break
+
+    else:
+        print('Número incorrecto')
+
