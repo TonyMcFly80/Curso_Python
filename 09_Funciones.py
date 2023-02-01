5d796cbfe20509a7715366472294ef635b34b867
TonyMcFly80 <123588199+TonyMcFly80@users.noreply.github.com>
2023-02-01 10:33:47 +0100

    Update 09_Funciones.py

--- 09_Funciones.py
+++ 09_Funciones.py
@@ -18,3 +18,27 @@ letra = str(input('Escribe una letra: '))
 
 print(f'En la frase \"{frase}\" la letra \"{letra}\" aparace \"{contar_letras(fr=frase, le=letra)}\" veces.')
 
+# Tarea 02
+# Vamos a crear una función que muestre por pantalla el triángulo de Pascal. Por parámetro se
+# indicará el número de filas a mostrar. Por defecto n valdrá 1.
+# Para calcular el número combinatorio en Python, utilizaremos el siguiente método:
+
+def combi(n, k):       # Calcula el número combinatorio n sobre k
+    import math
+
+    if (n >= k and k >= 0):
+        return math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
+
+    else:
+        return 'No se puede calcular el número factorial indicado'
+
+def trian_pascal(n = 1):
+    print(1)
+
+    for i in range(1, n + 1):
+        for k in range(i + 1):
+            print(combi(i, k), end=' ')
+        print('')
+
+
+trian_pascal(5)
