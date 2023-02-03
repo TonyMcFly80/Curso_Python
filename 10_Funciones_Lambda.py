3773329601ccc0aa8625c7bf12de5a07c5bc4695
TonyMcFly80 <123588199+TonyMcFly80@users.noreply.github.com>
2023-02-03 12:50:36 +0100

    Update 10_Funciones_Lambda.py

--- 10_Funciones_Lambda.py
+++ 10_Funciones_Lambda.py
@@ -2,6 +2,7 @@
 # NOTA: No es necesario usar código para comprobar si los datos introducidos por el usuario son correctos.
 # Vamos a interpretar que así es por ahora a menos que se especifique lo contrario.
 
+##############################################################
 
 # Tarea 01
 # Vamos a crear una función Lambda que devuelva una tupla donde en la primera posición esté
@@ -10,4 +11,16 @@
 operation = lambda x: (x, x*2, x**2)
 
 print(operation(5))
+
+##############################################################
+
+# Tarea 02
+# Dada una lista numérica, vamos a filtrarla y quedarnos con los números positivos. El resultado lo
+# mostraremos en una lista.
+
+nums = [0, 22, -4, 8, 9, -34, -1, 3, -8, 1]
+
+print(list(filter(lambda x: (x > 0), nums)))
+
+##############################################################
 
