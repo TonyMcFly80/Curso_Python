7ee9e48f73224d32b28471e5f1d5302d832d6c59
TonyMcFly80 <123588199+TonyMcFly80@users.noreply.github.com>
2023-02-03 12:24:01 +0100

    Update 10_Funciones_Lambda.py

--- 10_Funciones_Lambda.py
+++ 10_Funciones_Lambda.py
@@ -2,6 +2,12 @@
 # NOTA: No es necesario usar código para comprobar si los datos introducidos por el usuario son correctos.
 # Vamos a interpretar que así es por ahora a menos que se especifique lo contrario.
 
+
 # Tarea 01
 # Vamos a crear una función Lambda que devuelva una tupla donde en la primera posición esté
 # el número introducido por parámaetro; en la segunda, su doble; y, en la tercera, su cuadrado.
+
+operation = lambda x: (x, x*2, x**2)
+
+print(operation(5))
+
