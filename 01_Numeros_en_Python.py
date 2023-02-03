07299b58a6c48b7c449358ac461d01771fac3bca
TonyMcFly80 <123588199+TonyMcFly80@users.noreply.github.com>
2023-02-03 12:52:09 +0100

    Update 01_Numeros_en_Python.py

--- 01_Numeros_en_Python.py
+++ 01_Numeros_en_Python.py
@@ -1,16 +1,22 @@
 # EXAMEN Números en Python (Reales e imaginarios)
 
+##############################################################
+
 # Ejercicio 1
 # Calcula la división entera de 45 entre 6
 
 z = 45 / 6
 print(z)
 
+##############################################################
+
 # Ejercicio 2
 # Calcula el resto de la división entera de 45 entre 6
 
 z = 45 % 6
 print(z)
+
+##############################################################
 
 # Ejercicio 3
 # Realiza la siguiente operación en Python, donde ÷ indica la división entera:
@@ -19,17 +25,23 @@ print(z)
 z = 10 + 20 / 7 - 2
 print(z)
 
+##############################################################
+
 # Ejercicio 4
 # Realiza la siguiente operación en Python... y pásala a formaro python
 
 z = (9 - (25+5-2) / (7*4)) / 2**3
 print(z)
 
+##############################################################
+
 # Ejercicio 5
 # Realiza la siguiente operación en Python... y pásala a formaro python
 
 z = (2 + 2**3 - 2 * (2 - 2**5) + 2**2 * 2 + 2) / (2 * (2 * 2 - 2**4) + 2**2)
 print(z)
+
+##############################################################
 
 # Ejercicio 6
 # Realiza la siguiente operación en Python:
@@ -38,17 +50,23 @@ print(z)
 z = 6j - (4 + 1j) * 2
 print(z)
 
+##############################################################
+
 # Ejercicio 7
 # Realiza la siguiente operación en Python... y pásala a formaro python
 
 z = ((1 + 1j) / (1 - 1j)) + (2 / (-1 + 1j))
 print(z)
 
+##############################################################
+
 # Ejercicio 8
 # Realiza la siguiente operación en Python... y pásala a formato python
 
 z = (1 + 1j)**2
 print(z)
+
+##############################################################
 
 # Ejercicio 9
 # Realiza la siguiente operación en Python, donde Mod(z) indica módulo del complejo z
@@ -57,6 +75,8 @@ print(z)
 z = (9 - 3j) / (-2 - 1j)
 print(abs(z))
 
+##############################################################
+
 # Ejercicio 10
 # ¿Cuál es el argumento del número complejo i?:
 
@@ -64,3 +84,4 @@ import cmath
 z = 1j
 print(cmath.phase(z))
 
+##############################################################
