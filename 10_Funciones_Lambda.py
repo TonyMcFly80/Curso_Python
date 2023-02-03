b7683dff519d3edf08b79cdf1cc725b4f295ae61
TonyMcFly80 <123588199+TonyMcFly80@users.noreply.github.com>
2023-02-03 12:13:53 +0100

    Create 10_Funciones_Lambda.py

---
+++ 10_Funciones_Lambda.py
@@ -0,0 +1,7 @@
+# EJERCICIOS y EXAMEN FINAL del Tema
+# NOTA: No es necesario usar código para comprobar si los datos introducidos por el usuario son correctos.
+# Vamos a interpretar que así es por ahora a menos que se especifique lo contrario.
+
+# Tarea 01
+# Vamos a crear una función Lambda que devuelva una tupla donde en la primera posición esté
+# el número introducido por parámaetro; en la segunda, su doble; y, en la tercera, su cuadrado.
