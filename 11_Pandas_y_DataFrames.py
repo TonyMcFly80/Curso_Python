1bf720736069c551ebb55d971ff844aa2e74b4f2
TonyMcFly80 <123588199+TonyMcFly80@users.noreply.github.com>
2023-02-07 16:31:49 +0100

    Update 11_Pandas_y_DataFrames.py

--- 11_Pandas_y_DataFrames.py
+++ 11_Pandas_y_DataFrames.py
@@ -189,3 +189,22 @@ df_length.remove(6)
 print(df_length)
 
 ##############################################################
+
+# EXAMEN DEL TEMA
+
+##############################################################
+
+# Ejercicio 01
+# Crea un dataframe con 15 filas y 2 columnas. La primera columna se llamará x, la segunda y.
+# Cada entrada será un número real a tu elección. Guarda el dataframe en una variable llamada points.
+
+import pandas as pd
+
+data = {'x': [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30],
+        'y': [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29]}
+
+points = pd.DataFrame(data=data)
+
+print(points)
+
+##############################################################
