8e6b8893512a44de1fb714cc99d658131746ccb5
TonyMcFly80 <123588199+TonyMcFly80@users.noreply.github.com>
2023-02-07 17:15:26 +0100

    Update 11_Pandas_y_DataFrames.py

--- 11_Pandas_y_DataFrames.py
+++ 11_Pandas_y_DataFrames.py
@@ -222,3 +222,17 @@ points = pd.DataFrame(data=data)
 print(points[points['x'] > 0])
 
 ##############################################################
+
+# Ejercicio 03
+# Del dataset points, muestra las filas cuyo valor en la columna y sea negativo. Usa el método .query().
+
+import pandas as pd
+
+data = {'x': [2, -4, 6, -8, 10, 12, 14, -16, 18, -20, 22, 24, 26, 28, -30],
+        'y': [1, 3, -5, 7, 9, -11, 13, 15, -17, 19, 21, -23, 25, 27, 29]}
+
+points = pd.DataFrame(data=data)
+
+print(points.query('y < 0'))
+
+##############################################################
