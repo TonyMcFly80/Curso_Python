7b46669f758348c7253ee3dd879e47976d44c2e0
TonyMcFly80 <123588199+TonyMcFly80@users.noreply.github.com>
2023-02-09 10:00:21 +0100

    Update 12_OOP.py

--- 12_OOP.py
+++ 12_OOP.py
@@ -21,3 +21,36 @@ class RationalNumber():
             print('ERROR... Ambos números deben ser números enteros')
 
 ##############################################################
+
+# Tarea 02
+# Vamos a configurar el método .__str__() para que nos muestre el número racional de la forma
+# numerador / denominador.
+# EXTRA: Busca como usar LaTeX en strings de Python y construye el método .mathMode() que muestre
+# el número racional en formato numerator
+#                               ---------
+#                               denominator
+
+class RationalNumber():
+    """
+    Clase para trabajar con números racionales.
+    """
+    def __init__(self, numerator, denominator=1):
+        if type(numerator) is int and type(denominator) is int:
+            self.numerator = numerator
+            self.denominator = denominator
+        else:
+            print('ERROR... Ambos números deben ser números enteros')
+
+    def __str__(self):
+        return f'{self.numerator} / {self.denominator}'
+
+    def mathFormat(self):
+        from IPython.display import display, Latex
+        display(Latex(f'${self.numerator}\\over{self.denominator}$'))
+
+
+q = RationalNumber(12, 8)
+print(q)
+q.mathFormat()
+
+##############################################################
